a6412ef11d6ebf87eb867d60769cd08f96f941dc
Kenz321818 <149667575+Kenz321818@users.noreply.github.com>
2024-02-04 19:11:10 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
 global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
-global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.MONGO_DB_URI = "mongodb+srv://kenzodika972:<password>@bot.esnbvcv.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
+global.ACTIVATION_TOKEN_SECRET = "kenzofcc" //isi apa aja bebas
+global.your_email = "kenzodika972@gmail.com" //email
+global.email_password = "ghlk fqmd wouu oiwz" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
